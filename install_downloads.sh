6e48ad5fd3cd11f22e6a4da533689de659d78c67
the-braveknight <zaid.riadh@gmail.com>
2016-08-18 01:19:52 +0300

    make sure AppleHDA_CX20751_Resources .zml.zlib files are created

--- install_downloads.sh
+++ install_downloads.sh
@@ -196,6 +196,7 @@ HDA=CX20751
 $SUDO rm -Rf $KEXTDEST/AppleHDA_$HDA.kext
 $SUDO rm -Rf $KEXTDEST/AppleHDAHCD_$HDA.kext
 $SUDO rm -f $SLE/AppleHDA.kext/Contents/Resources/*.zml*
+./patch_hda.sh "$HDA"
 if [[ 0 -eq 1 ]]; then
 # dummyHDA configuration
 make AppleHDA_$HDA.kext
