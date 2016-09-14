8a74088349658613b997f407554183b46104b4cc
the-braveknight <zaid.riadh@gmail.com>
2016-09-14 13:47:33 +0300

    disable CodecCommander.kext, use AppleHDAHCD kext instead

--- install_downloads.sh
+++ install_downloads.sh
@@ -204,7 +204,7 @@ if [[ $MINOR_VER -le 9 ]]; then
 else
     # alternate configuration (requires .xml.zlib .zml.zlib AppleHDA patch)
     #make AppleHDAHCD_$HDA.kext
-    #install_kext AppleHDAHCD_$HDA.kext
+    install_kext AppleHDAHCD_$HDA.kext
     $SUDO cp AppleHDA_${HDA}_Resources/*.zml* $SLE/AppleHDA.kext/Contents/Resources
     $TAG -a Gray $SLE/AppleHDA.kext
 fi
