0d292ad449d3bf4f8763519f79a82cee2aa02752
Zaid Rahawi <zaid_riadh@yahoo.com>
2016-09-27 19:17:52 +0300

    minor rework

--- prepare_usb.sh
+++ prepare_usb.sh
@@ -2,20 +2,19 @@
 
 # Written by the-braveknight, based on RehabMan's scripts.
 
-# Note: this script requires 'make', download.sh & install_downloads.sh
-# scripts to have been run & Clover to have been installed prior to running it.
-
-# Note: this script assumes the USB drive is 'disk1' (/dev/disk1) that is
-# partitioned in GPT.
+# Note: this script requires download.sh & install_downloads.sh scripts
+# to have been run & assumes Clover has already been installed to
+# the USB drive which is 'disk1' (/dev/disk1) that is partitioned in GPT.
 
 SUDO=sudo
 EFI=`$SUDO ./mount_efi.sh /dev/disk1`
-KEXTDEST=$EFI/EFI/CLOVER/kexts/Other
+CLOVER=$EFI/EFI/CLOVER
+KEXTDEST=$CLOVER/kexts/Other
 KEXTSDIR=./kexts
 AKEXTSDIR=./downloads/kexts
-config=$EFI/EFI/Clover/config.plist
-
+CONFIG=$CLOVER/config.plist
 BUILDDIR=./build
+PlistBuddy=/usr/libexec/plistbuddy
 
 function install_kext
 {
@@ -25,6 +24,17 @@ function install_kext
         $SUDO cp -Rf $1 $KEXTDEST
     fi
 }
+
+if [ ! -e $AKEXTSDIR ]; then
+    echo "no kexts found in $AKEXTSDIR, please run download.sh & install_downloads.sh scripts."
+    exit
+fi
+
+
+if [ ! -e $BUILDDIR/SSDT-HACK.aml ]; then
+    echo "no SSDT-HACK.aml found in $BUILDDIR, compiling SSDT-HACK.dsl..."
+    iasl -ve -p ./build/SSDT-HACK.aml SSDT-HACK.dsl
+fi
 
 # Install kexts in the repo
 cd $KEXTSDIR
@@ -40,39 +50,41 @@ cd RehabMan-Realtek-Network-v2*/Release && install_kext RealtekRTL8111.kext && c
 cd RehabMan-Battery*/Release && install_kext ACPIBatteryManager.kext && cd ..//..
 cd ..//..
 
+
 # Copy SSDT-HACK.aml from ./build to CLOVER/ACPI/patched
-echo copying $BUILDDIR/SSDT-HACK.aml to $EFI/EFI/CLOVER/ACPI/patched
-cp $BUILDDIR/SSDT-HACK.aml $EFI/EFI/CLOVER/ACPI/patched
+echo copying $BUILDDIR/SSDT-HACK.aml to $CLOVER/ACPI/patched
+cp $BUILDDIR/SSDT-HACK.aml $CLOVER/ACPI/patched
+
 
 
 # Download & copy HFSPlus.efi from CloverGrowerPro repo to CLOVER/drivers64UEFI
 echo downloading HFSPlus.efi
 curl https://raw.githubusercontent.com/JrCs/CloverGrowerPro/master/Files/HFSPlus/X64/HFSPlus.efi -o HFSPlus.efi --progress-bar
-echo copying HFSPlus.efi to $EFI/EFI/CLOVER/drivers64UEFI
-cp ./HFSPlus.efi $EFI/EFI/CLOVER/drivers64UEFI
+echo copying HFSPlus.efi to $CLOVER/drivers64UEFI
+cp ./HFSPlus.efi $CLOVER/drivers64UEFI
 
 
 # Copy config_install.plist from the repo to Clover folder
-echo copying config.plist to $EFI/EFI/CLOVER/config.plist
-cp config.plist $EFI/EFI/CLOVER/config.plist
+echo copying config.plist to $CLOVER
+cp config.plist $CLOVER
 
-# Remove unnecessary patches from config.plist
-PlistBuddy=/usr/libexec/plistbuddy
 
-echo removing HDA patches from config.plist
-$PlistBuddy -c "Delete ':Devices:Arbitrary:1'" $config
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:3'" $config
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:1'" $config
+# Remove unnecessary patches from Clover/config.plist
 
-echo removing HDMI patches from config.plist
-$PlistBuddy -c "Delete ':Devices:Arbitrary:1'" $config
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:0'" $config
+echo removing HDA patches from $CONFIG
+$PlistBuddy -c "Delete ':Devices:Arbitrary:1'" $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:3'" $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:1'" $CONFIG
 
-echo removing WiFi/BT patches from config.plist
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:0'" $config
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:6'" $config
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:5'" $config
-$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:4'" $config
+echo removing HDMI patches from $CONFIG
+$PlistBuddy -c "Delete ':Devices:Arbitrary:1'" $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:0'" $CONFIG
+
+echo removing WiFi/BT patches from $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:0'" $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:6'" $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:5'" $CONFIG
+$PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:4'" $CONFIG
 
 
 
@@ -80,11 +92,11 @@ $PlistBuddy -c "Delete ':KernelAndKextPatches:KextsToPatch:4'" $config
 diskutil unmount $EFI
 HDEFI=`$SUDO ./mount_efi.sh /`
 if [ -e $HDEFI/EFI/CLOVER/smbios.plist ]; then
-    echo smbios.plist exists, copying to $EFI/EFI/CLOVER
+    echo smbios.plist exists, copying to $CLOVER
     cp $HDEFI/EFI/CLOVER/smbios.plist /tmp/smbios.plist
     diskutil unmount $HDEFI
     EFI=`$SUDO ./mount_efi.sh /dev/disk1`
-    cp /tmp/smbios.plist $EFI/EFI/CLOVER
+    cp /tmp/smbios.plist $CLOVER
 else
     diskutil unmount $HDEFI
     EFI=`$SUDO ./mount_efi.sh /dev/disk1`
