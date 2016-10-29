3aad2a86a628dc5faed9ff4a02211abecc602613
Zaid Rahawi <zaid_riadh@yahoo.com>
2016-10-29 14:59:56 +0300

    cleanup update_config.sh script; remove serial number generation

--- update_config.sh
+++ update_config.sh
@@ -3,65 +3,22 @@
 #set -x
 
 EFI=`sudo ./mount_efi.sh /`
-config=$EFI/EFI/Clover/config.plist
-#config=config_new.plist
-
-# smbios data
-# Note: The code for serial# generation from ProBook Installer, originally
-#  researched/written by RehabMan and philip_petev
-
-week=CDFGHJKLMNPQRTVWXY12345678
-week2=012345678
-chars=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
-week_letter=`echo ${week:$(($RANDOM%${#week})):1}`
-week_letter2=`echo ${week:$(($RANDOM%${#week2})):2}`
-
-function random_char()
-{
-    echo ${chars:$(($RANDOM%${#chars})):1};
-}
-
-function serial_number()
-{
-    echo "C02"$week_letter$(random_char)$(random_char)$(random_char)"F5V7"
-}
-
-function generate_new_plist()
-{
-    cp config.plist $config
-    echo Generated random serial: \"$(serial_number)\"
-    /usr/libexec/plistbuddy -c "Set :SMBIOS:SerialNumber $(serial_number)" $config
-}
-
-# no config.plist... make new one.
-if [[ ! -e $config ]]; then
-    echo No config.plist at $config, generating new.
-    generate_new_plist
-    exit
-fi
-
-# check to see if it is installation config.plist... if so, make new one
-check=`/usr/libexec/plistbuddy -c "Print :ACPI:SortedOrder-Comment" $config 2>&1`
-if [[ ! "$check" == *"Does Not Exist"* ]]; then
-    echo The config.pilst at $config is install plist, generating new.
-    generate_new_plist
-    exit
-fi
+CONFIG=$EFI/EFI/Clover/config.plist
 
 function replace_var()
 # $1 is path to replace
 {
-    value=`/usr/libexec/plistbuddy -c "Print \"$1\"" $config`
-    /usr/libexec/plistbuddy -c "Set \"$1\" \"$value\"" $config
+    value=`/usr/libexec/plistbuddy -c "Print \"$1\"" $CONFIG`
+    /usr/libexec/plistbuddy -c "Set \"$1\" \"$value\"" $CONFIG
 }
 
 function replace_dict()
 # $1 is path to replace
 {
     /usr/libexec/plistbuddy -x -c "Print \"$1\"" config.plist >/tmp/org_rehabman_node.plist
-    /usr/libexec/plistbuddy -c "Delete \"$1\"" $config
-    /usr/libexec/plistbuddy -c "Add \"$1\" dict" $config
-    /usr/libexec/plistbuddy -c "Merge /tmp/org_rehabman_node.plist \"$1\"" $config
+    /usr/libexec/plistbuddy -c "Delete \"$1\"" $CONFIG
+    /usr/libexec/plistbuddy -c "Add \"$1\" dict" $CONFIG
+    /usr/libexec/plistbuddy -c "Merge /tmp/org_rehabman_node.plist \"$1\"" $CONFIG
 }
 
 # existing config.plist, preserve:
@@ -80,7 +37,7 @@ function replace_dict()
 #   RtVariables:BooterConfig
 #   RtVariables:CsrActiveConfig
 
-echo The config.plist at $config will be updated.
+echo The config.plist at $CONFIG will be updated.
 
 replace_dict ":ACPI"
 replace_dict ":Boot"
@@ -89,4 +46,3 @@ replace_dict ":KernelAndKextPatches"
 replace_dict ":SystemParameters"
 replace_var ":RtVariables:BooterConfig"
 replace_var ":RtVariables:CsrActiveConfig"
-
