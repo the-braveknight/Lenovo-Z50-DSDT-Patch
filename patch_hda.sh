4275c329ee5fae0b9983f5f282175ccb0132d39f
the-braveknight <zaid.riadh@gmail.com>
2017-12-07 16:41:18 +0300

    Minor changes to new patch_hda.sh

--- patch_hda.sh
+++ patch_hda.sh
@@ -2,90 +2,117 @@
 
 # Originally written by RehabMan, reworked/simplified by the-braveknight.
 
+hda_native=/System/Library/Extensions/AppleHDA.kext
+hcd_native=$hda_native/Contents/PlugIns/AppleHDAHardwareConfigDriver.kext
+
 function fixVersion() {
     oldValue=$(/usr/libexec/PlistBuddy -c "Print $1" $2)
     newValue=$(echo $oldValue | perl -p -e 's/(\d*\.\d*(\.\d*)?)/9\1/')
     /usr/libexec/PlistBuddy -c "Set $1 '$newValue'" $2
 }
 
-# Create AppleHDA PathMaps/Layouts injector kext
-function createAppleHDAInjector() {
-    native=/System/Library/Extensions/AppleHDA.kext
-    injector=AppleHDA_$1.kext
-
-    echo "Creating $injector"
-
-    rm -Rf $injector && mkdir $injector && mkdir $injector/Contents && mkdir $injector/Contents/Resources && mkdir $injector/Contents/MacOS
-    cp $native/Contents/Info.plist $injector/Contents/Info.plist
-    ln -s $native/Contents/MacOS/AppleHDA $injector/Contents/MacOS/AppleHDA
-
-    for layout in Resources_$1/layout*.plist; do
-        ./tools/zlib deflate $layout > $injector/Contents/Resources/$(basename $layout .plist).xml.zlib
-    done
-
-    ./tools/zlib inflate $native/Contents/Resources/Platforms.xml.zlib > /tmp/Platforms.plist
-    /usr/libexec/PlistBuddy -c "Delete ':PathMaps'" /tmp/Platforms.plist
-    /usr/libexec/PlistBuddy -c "Merge Resources_$1/Platforms.plist" /tmp/Platforms.plist
-    ./tools/zlib deflate /tmp/Platforms.plist > $injector/Contents/Resources/Platforms.xml.zlib
-
-    fixVersion ":NSHumanReadableCopyright" $injector/Contents/Info.plist
-    fixVersion ":CFBundleVersion" $injector/Contents/Info.plist
-    fixVersion ":CFBundleGetInfoString" $injector/Contents/Info.plist
-    fixVersion ":CFBundleShortVersionString" $injector/Contents/Info.plist
-}
-
-# Create AppleHDAHCD PinConfigs injector kext
+# AppleHDAHCD PinConfigs injector kext
 function createAppleHDAHCDInjector() {
-    native=/System/Library/Extensions/AppleHDA.kext/Contents/PlugIns/AppleHDAHardwareConfigDriver.kext
-
-    # If AppleHDA PathMaps/Layouts injector kext exists, include AppleHDAHCD injector in its PlugIns
-    if [ -d AppleHDA_$1.kext/Contents ]; then
-        if [ ! -d AppleHDA_$1.kext/Contents/PlugIns ]; then
-            mkdir AppleHDA_$1.kext/Contents/PlugIns
-        fi
-        injector=AppleHDA_$1.kext/Contents/PlugIns/AppleHDAHCD_$1.kext
+    if [[ -d $2 ]]; then
+        hcd_injector=$2/AppleHDAHCD_$1.kext
     else
-        injector=AppleHDAHCD_$1.kext
+        hcd_injector=AppleHDAHCD_$1.kext
     fi
 
-    echo "Creating $injector"
+    echo "Creating $hcd_injector"
 
-    rm -Rf $injector && mkdir $injector && mkdir $injector/Contents
-    cp $native/Contents/Info.plist $injector/Contents/Info.plist
+    rm -Rf $hcd_injector && mkdir $hcd_injector && mkdir $hcd_injector/Contents
+    cp $hcd_native/Contents/Info.plist $hcd_injector/Contents/Info.plist
 
-    fixVersion ":NSHumanReadableCopyright" $injector/Contents/Info.plist
-    fixVersion ":CFBundleVersion" $injector/Contents/Info.plist
-    fixVersion ":CFBundleGetInfoString" $injector/Contents/Info.plist
-    fixVersion ":CFBundleShortVersionString" $injector/Contents/Info.plist
+    fixVersion ":NSHumanReadableCopyright" $hcd_injector/Contents/Info.plist
+    fixVersion ":CFBundleVersion" $hcd_injector/Contents/Info.plist
+    fixVersion ":CFBundleGetInfoString" $hcd_injector/Contents/Info.plist
+    fixVersion ":CFBundleShortVersionString" $hcd_injector/Contents/Info.plist
 
-    /usr/libexec/PlistBuddy -c "Delete ':BuildMachineOSBuild'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTCompiler'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTPlatformBuild'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTPlatformVersion'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTSDKBuild'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTSDKName'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTXcode'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':DTXcodeBuild'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':OSBundleLibraries'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':CFBundleExecutable'" $injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':BuildMachineOSBuild'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTCompiler'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTPlatformBuild'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTPlatformVersion'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTSDKBuild'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTSDKName'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTXcode'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':DTXcodeBuild'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':OSBundleLibraries'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':CFBundleExecutable'" $hcd_injector/Contents/Info.plist
 
-    /usr/libexec/PlistBuddy -c "Set ':CFBundleIdentifier' 'org.the-braveknight.AppleHDAHCDInjector'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Set ':CFBundleName' 'AppleHDAHCDInjector'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':IOKitPersonalities:HDA Hardware Config Resource:PostConstructionInitialization'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Delete ':IOKitPersonalities:HDA Hardware Config Resource:HDAConfigDefault'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Merge ./Resources_$1/ahhcd.plist ':IOKitPersonalities:HDA Hardware Config Resource'" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Add ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' integer" $injector/Contents/Info.plist
-    /usr/libexec/PlistBuddy -c "Set ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' 2000" $injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Set ':CFBundleIdentifier' 'org.the-braveknight.AppleHDAHCDInjector'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Set ':CFBundleName' 'AppleHDAHCDInjector'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':IOKitPersonalities:HDA Hardware Config Resource:PostConstructionInitialization'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Delete ':IOKitPersonalities:HDA Hardware Config Resource:HDAConfigDefault'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Merge ./Resources_$1/ahhcd.plist ':IOKitPersonalities:HDA Hardware Config Resource'" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Add ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' integer" $hcd_injector/Contents/Info.plist
+    /usr/libexec/PlistBuddy -c "Set ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' 2000" $hcd_injector/Contents/Info.plist
 }
 
-if [ "$1" == "" ]; then
+# Generate AppleHDA layouts/pathmaps resources
+function createAppleHDAResources() {
+    if [[ -d $2 ]]; then
+        resources=$2
+    else
+        resources=AppleHDA_$1_Resources
+    fi
+
+    if [[ "$3" == *"-xml" ]]; then
+        extension=xml
+    else
+        extension=zml
+    fi
+
+    rm -Rf $resources && mkdir $resources
+
+    for layout in Resources_$1/layout*.plist; do
+        ./tools/zlib deflate $layout > $resources/$(basename $layout .plist).$extension.zlib
+    done
+
+    ./tools/zlib inflate $hda_native/Contents/Resources/Platforms.xml.zlib > /tmp/Platforms.plist
+    /usr/libexec/PlistBuddy -c "Delete ':PathMaps'" /tmp/Platforms.plist
+    /usr/libexec/PlistBuddy -c "Merge Resources_$1/Platforms.plist" /tmp/Platforms.plist
+    ./tools/zlib deflate /tmp/Platforms.plist > $resources/Platforms.$extension.zlib
+}
+
+# Create AppleHDA layouts/pathmaps injector kext
+function createAppleHDALayoutsInjector() {
+    if [[ -d $2 ]]; then
+        hda_injector=$2/AppleHDA_$1.kext
+    else
+        hda_injector=AppleHDA_$1.kext
+    fi
+
+    echo "Creating $hda_injector"
+
+    rm -Rf $hda_injector && mkdir $hda_injector && mkdir $hda_injector/Contents && mkdir $hda_injector/Contents/Resources && mkdir $hda_injector/Contents/MacOS
+    cp $hda_native/Contents/Info.plist $hda_injector/Contents/Info.plist
+    ln -s $hda_native/Contents/MacOS/AppleHDA $hda_injector/Contents/MacOS/AppleHDA
+
+    createAppleHDAResources $1 $hda_injector/Contents/Resources -xml
+
+    fixVersion ":NSHumanReadableCopyright" $hda_injector/Contents/Info.plist
+    fixVersion ":CFBundleVersion" $hda_injector/Contents/Info.plist
+    fixVersion ":CFBundleGetInfoString" $hda_injector/Contents/Info.plist
+    fixVersion ":CFBundleShortVersionString" $hda_injector/Contents/Info.plist
+}
+
+# Create all-in-one AppleHDA injector kext
+function createAppleHDAInjector() {
+    hda_injector=AppleHDA_$1.kext
+    createAppleHDALayoutsInjector "$1"
+    mkdir $hda_injector/Contents/PlugIns
+    createAppleHDAHCDInjector "$1" $hda_injector/Contents/PlugIns
+}
+
+if [ ! -d Resources_$1 ]; then
     echo Usage: patch_hda.sh {codec}
     echo Example: patch_hda.sh CX20751
     exit 1
 fi
 
-rm -Rf AppleHDA_$1.kext && rm -Rf AppleHDAHCD_$1.kext
-
-createAppleHDAInjector "$1"
+#createAppleHDAInjector "$1"
+createAppleHDALayoutsInjector "$1"
 #createAppleHDAHCDInjector "$1"
+#createAppleHDAResources "$1"
 
